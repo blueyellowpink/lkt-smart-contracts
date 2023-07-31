c51db75e57bbae05d00adb7d704c656b23cfd78c
blueyellowpink <andrew@trasua.dev>
2023-07-31 20:25:32 +0700

    token transfer

--- tasks/token.js
+++ tasks/token.js
@@ -1,17 +1,19 @@
 const { task, types } = require("hardhat/config")
+const { parseLkt } = require("../utils")
 
 const tokenAbi = require("../artifacts/contracts/Token.sol/Token.json").abi
 
-task("token", "transfer").setAction(async (args, hre) => {
-    const { LKT } = require(`../bsc_testnet_addresses.json.json`)
-
+task("token-transfer", "Transfer token").setAction(async (args, hre) => {
+    const { LKT  } = require(`../bsc_${hre.network.name}_addresses.json`)
     const signer = await ethers.getSigner()
+    const token = new ethers.Contract(LKT, tokenAbi, signer)
 
-    const character = new ethers.Contract(LKT, tokenAbi, signer)
+    const params = {
+        amount: parseLkt('10'),
+        address: '0x61e011cc5f4E7D17BF5949B33773dbC3A32CbFD8'
+    }
+    const tx = await token.transfer(params.address, params.amount)
+    await tx.wait()
 
-    // const address = '0xAe454040CEC4403aBe10C1f9BA7e92907616C7b2'
-    // const tx = await setMinter(character, address)
-    //
-    // txUrl(hre.network.name, tx)
-    // console.log('done')
+    console.log('done')
 })
