ea9f8b241e3932607f09e05378c9f411eb37a126
blueyellowpink <andrew@trasua.dev>
2023-09-22 19:01:56 +0700

    token burnable

--- verify_token.sh
+++ verify_token.sh
@@ -2,6 +2,6 @@
 
 NETWORK=testnet
 ARGS=arguments_lkt.js
-ADDRESS=0xfb4488AF459Ddd56CaBbC5F8544Fc2E3cb840d2A
+ADDRESS=0xF051904AecB6A32Ce3d904aFA7BE20C7c8961D04
 
 yarn run hardhat verify --network $NETWORK --constructor-args ./verify/$ARGS --no-compile $ADDRESS
